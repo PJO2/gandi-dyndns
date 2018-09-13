1e4dd352a16c368815e4948272ba577bef695d0f
PJO2 <web@jounin.net>
2018-09-13 13:20:51 +0200

    creation

---
+++ gandi-dyndns.sh
@@ -0,0 +1,12 @@
+#
+# update gandi dns with box ip
+# to be installed into crontab with contab -e
+# add 0,15,30,45 * * * * ./gandi-dyndns.sh > /dev/null 2> /dev/null
+#
+curl -s  http://api.ipify.org | 
+     xargs -I {} curl -H 'Content-type: application/json' \
+                      -H 'x-api-key: XXXXXXXXXXXXXX' \
+                      -X PUT \
+                      -d '{ "rrset_ttl": 1800, "rrset_values": ["{}"] }' \
+                       https://dns.api.gandi.net/api/v5/domains/<my domain>/records/<my cname entry>/A
+  
